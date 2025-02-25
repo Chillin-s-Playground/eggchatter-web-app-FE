1ba682a518edfcf3addb74c35a468eb25c1eb0a6
chilln <kimjihan77@naver.com>
2025-02-25 23:37:53 +0900

    feat:패키지 설치 - axios

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
 import type { NextConfig } from "next";
 
+
+const giphyDomains = [
+  'media0.giphy.com', 
+  'media1.giphy.com', 
+  'media2.giphy.com', 
+  'media3.giphy.com', 
+  'media4.giphy.com', 
+  'giphy.com'
+];
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     formats: ['image/webp'],
-  },
+    domains: giphyDomains
+  }
 };
 
 export default nextConfig;
