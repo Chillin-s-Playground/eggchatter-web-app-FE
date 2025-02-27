add56b14267c83bb2aa46449363d17127a360b24
chilln <kimjihan77@naver.com>
2025-02-27 16:55:56 +0900

    fix:페이지인디케이터 우측메뉴 props 추가

--- components/PageIndicator.tsx
+++ components/PageIndicator.tsx
@@ -1,10 +1,30 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from "@mui/material/IconButton";
+
 interface PageIndicatorProps {
     text :string;
+    hasMenu ?: boolean
+    handleClickMenu ?: () => void
 }
 
-const PageIndicator = ({ text } : PageIndicatorProps) => {
+const PageIndicator = ({ text, hasMenu, handleClickMenu } : PageIndicatorProps) => {
     return (
-        <p>{text}</p>
+        <div className='flex justify-between items-center w-full'>
+            <p className='text-xl'>{text}</p>
+
+            {hasMenu && (
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    onClick={handleClickMenu}
+                >
+                <MenuIcon />
+              </IconButton>
+            )}
+        </div>
     )
 }
 
